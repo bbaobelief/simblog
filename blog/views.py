35bf96ab96002d311d56e1ba8b7ae912695b8d08
zhengfuqiang <zhengfuqiang_d@duoku.com>
2016-01-07 15:54:11 +0800

    del Get_Category

--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,28 @@
 # -*- coding: utf-8 -*-
-from base.mixins.views import SimblogListView
+from base.mixins.views import SimblogListView,SimblogDetailView
 
 from .models import *
 
-def Get_Category(lis):
-    cate_list = []
-    for c in lis:
-        if lis.index(c) >= len(lis)/2:
-          print c,"++++"
-          return cate_list.append(c)
-        else:
-          print c,"----"
-          return cate_list.append(c)
-
-lis = ['python','django','python','nosql','life','web']
-
-class ProjectList(SimblogListView):
-    model = User
-    template_name = 'blog/page-blog.html'
-    context_object_name = 'projects'
+class BlogList(SimblogListView):
+    model = Article
+    template_name = 'blog/blog_list.html'
+    context_object_name = 'article'
 
     def get_context_data(self, **kwargs):
-        context = super(ProjectList, self).get_context_data(**kwargs)
-        context['categories'] = lis
+        context = super(BlogList, self).get_context_data(**kwargs)
+        context['categories'] = ['python','django','python','nosql','life','web','uuuu','234234']
+        return context
+
+
+class BlogDetail(SimblogDetailView):
+    model = Article
+    template_name = 'blog/blog_detail.html'
+    context_object_name = 'article'
+
+    def get_context_data(self, **kwargs):
+        context = super(BlogDetail, self).get_context_data(**kwargs)
+        # context['breadcrumb'].extend([
+        #     (u'项目总览', reverse_lazy('project_list')),
+        #     (self.object.name, ''),
+        # ])
         return context
