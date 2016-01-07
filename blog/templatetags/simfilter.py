2094d552bec274ec2a05535a95da85e6108622cf
zhengfuqiang <zhengfuqiang_d@duoku.com>
2016-01-07 15:51:52 +0800

    添加模版分类过滤器

--- blog/templatetags/simfilter.py
+++ blog/templatetags/simfilter.py
@@ -2,8 +2,16 @@
 from django import template
 register = template.Library()
 
-def lower(value): # Only one argument.
-    """Converts a string into all lowercase"""
-    return '%s@@'%value.upper()
-
-register.filter('lower', lower)
+@register.filter(name='Category_LR')
+def Category_LR(value,arg): # Only one argument.
+    """category as left or right"""
+    value_len = len(value)/2
+    left_value = value[0:value_len]
+    if (value_len % 2) == 0:
+       left_value = value[0:value_len]
+       right_value = value[value_len:len(value)]
+    else:
+       left_value = value[0:value_len+1]
+       right_value = value[value_len+1:len(value)]
+    category_dic = {'left_value':left_value, 'right_value':right_value}
+    return category_dic[arg]
