3e926e8b17e68e3474fe38a1b4790d821ea6e1da
zhengfuqiang <zhengfuqiang_d@duoku.com>
2016-01-07 15:53:25 +0800

    add ProjectList

--- blog/urls.py
+++ blog/urls.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 
 from django.conf.urls import url
-from .views import ProjectList
+from .views import BlogList,BlogDetail
 
 urlpatterns = [
     # Blog
-    url(r'^$', ProjectList.as_view(), name='project_list'),
-    url(r'^blog/$', ProjectList.as_view(), name='project_list'),
+    url(r'^$', BlogList.as_view(), name='blog_list'),
+    url(r'^list/$', BlogList.as_view(), name='blog_list'),
     # url(r'^project/add/$', ProjectCreate.as_view(), name='project_create'),
-    # url(r'project/(?P<pk>\d+)/$', ProjectDetail.as_view(), name='project_detail'),
+    url(r'^detail/(?P<pk>\d+)/$', BlogDetail.as_view(), name='blog_detail'),
     # url(r'^project/(?P<pk>\d+)/update/$', ProjectUpdate.as_view(), name='project_update'),
     # url(r'^project/(?P<pk>\d+)/delete/$', ProjectDelete.as_view(), name='project_delete'),
 
