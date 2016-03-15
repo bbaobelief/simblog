d20e3607e5386c68273965442b75b975fab7774d
zhengfuqiang <zhengfuqiang_d@duoku.com>
2016-03-15 11:21:17 +0800

    调整models字段

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 class Tag(models.Model):
     """标签"""
-    tag_name = models.CharField(max_length=20)
+    name = models.CharField(u'名称', max_length=20, unique=True)
     create_time = models.DateTimeField(auto_now_add=True)
 
     class Meta:
@@ -13,28 +13,30 @@ class Tag(models.Model):
         verbose_name_plural = verbose_name
 
     def __unicode__(self):
-        return self.tag_name
+        return self.name
 
 
 class Category(models.Model):
-    name = models.CharField(u'分类', max_length=20)
-    sorting = models.IntegerField(u'排序', default=10, blank=True)
+    """分类"""
+    name = models.CharField(u'分类', max_length=20, unique=True)
+    order = models.IntegerField(u'顺序', default=10, blank=True)
 
     class Meta:
         verbose_name = u'分类'
         verbose_name_plural = verbose_name
-        ordering = ['sorting', 'id']
+        ordering = ['order', 'id']
 
     def __unicode__(self):
         return self.name
 
 
 class Author(models.Model):
+    """作者"""
     user = models.OneToOneField(User)
     name = models.CharField(max_length=30)
     email = models.EmailField(blank=True)
     website = models.URLField(blank=True)
-    avatar = models.ImageField(u'头像', upload_to='uploads/avatar/%Y/%m', blank=True, default='uploads/avatar/default.png')
+    avatar = models.ImageField(upload_to='uploads/avatar/%Y/%m', blank=True, default='uploads/avatar/default.png')
 
     class Meta:
         verbose_name = u'作者'
@@ -45,17 +47,21 @@ class Author(models.Model):
 
 
 class Article(models.Model):
+    """文章"""
+    SOURCE_CHOICE = (
+                        ('original', u'原创'),
+                        ('reprint', u'转载')
+                    )
+
     title = models.CharField(u'标题', max_length=50)
     content = models.TextField(u'内容')
-    author = models.ForeignKey(Author, default=1, verbose_name=u'作者')
-    category = models.ForeignKey(Category, default=1, verbose_name=u'分类')
-    tags = models.ManyToManyField(Tag, blank=True, verbose_name=u'标签')
-    is_top = models.BooleanField(u'置顶', default=False)
     counts = models.IntegerField(u'点击率', default=0, blank=True)
-    show_type = (('True', u'显示'), ('False', u'隐藏'))
-    is_show = models.CharField(u'加密', max_length=100, choices=show_type, default='True')
-    source_type = (('yuanchuang', u'原创'), ('zhuanzai', u'转载'))
-    source = models.CharField(u'来源', max_length=30, choices=source_type, default='yuanchuang')
+    is_top = models.BooleanField(u'置顶', default=False)
+    is_show = models.BooleanField(u'加密', default=False)
+    author = models.ForeignKey(Author, default=1, related_name='author_article', verbose_name=u'作者')
+    category = models.ForeignKey(Category, default=1, related_name='category_article', verbose_name=u'分类')
+    tag = models.ManyToManyField(Tag, blank=True, related_name='tag_article', verbose_name=u'标签')
+    source = models.CharField(u'来源', max_length=30, choices=SOURCE_CHOICE, default='original')
     source_link = models.URLField(blank=True)
     publish_time = models.DateTimeField(u'发布时间', auto_now_add=True)
 
@@ -73,12 +79,12 @@ class Link(models.Model):
     name = models.CharField(max_length=30)
     email = models.EmailField(blank=True)
     website = models.URLField()
-    sorting = models.IntegerField(u'排序', default=0, blank=True)
+    order = models.IntegerField(u'顺序', default=0, blank=True)
 
     class Meta:
         verbose_name = u'友链'
         verbose_name_plural = verbose_name
-        ordering = ['sorting', 'id']
+        ordering = ['order', 'id']
 
     def __unicode__(self):
         return self.name
