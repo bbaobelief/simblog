f56ddc3aadcaf5d1a3b5d119d14fc95473b40eb7
zhengfuqiang <zhengfuqiang_d@duoku.com>
2016-03-15 11:30:07 +0800

    修改admin显示字段

--- blog/admin.py
+++ blog/admin.py
@@ -1,20 +1,18 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
-from models import *
+from models import Article, Author, Tag, Category, Link
 
 class AuthorAdmin(admin.ModelAdmin):
-    """docstring for AuthorAdmin"""
     list_display = ('name', 'email', 'website')
     search_fields = ('name',)
 
 class ArticleAdmin(admin.ModelAdmin):
-    """docstring for ArticleAdmin"""
     list_display = ('id','title','category','is_show','author','publish_time','is_top') #指定要显示的字段
     list_display_links = ('title', 'id', ) #指定可以点击的字段
     list_filter = ('publish_time',)        #指定列表过滤器，右边将会出现一个快捷的日期过滤选项，
     # date_hierarchy = 'publish_time'        #日期型字段进行层次划分
     ordering = ('-publish_time',)          #对日期降序排列
-    filter_horizontal = ('tags',)          #水平过滤器
+    filter_horizontal = ('tag',)          #水平过滤器
     list_editable = ('is_show','category')  #列表页可编辑
 
     # 分组表单
@@ -50,21 +48,18 @@ class ArticleAdmin(admin.ModelAdmin):
         )
 
 class CategoryAdmin(admin.ModelAdmin):
-    """docstring for CategoryAdmin"""
-    list_display = ('id', 'name', 'sorting')
+    list_display = ('id', 'name', 'order')
     search_fields = ('name',)
-    ordering = ('sorting',)          #对日期降序排列
-    list_editable = ('name', 'sorting')  #列表页可编辑
+    ordering = ('order',)          #对日期降序排列
+    list_editable = ('name', 'order')  #列表页可编辑
 
 class TagAdmin(admin.ModelAdmin):
-    """docstring for TagAdmin"""
-    list_display = ('id', 'tag_name', 'create_time')
+    list_display = ('id', 'name', 'create_time')
     search_fields = ('name',)
     ordering = ('create_time',)          #对日期降序排列
-    list_editable = ('tag_name', )  #列表页可编辑
+    list_editable = ('name', )  #列表页可编辑
 
 class LinkAdmin(admin.ModelAdmin):
-    """docstring for LinkAdmin"""
     list_display = ('id', 'name', 'email', 'website')
     search_fields = ('name',)
     ordering = ('name',)          #对日期降序排列
