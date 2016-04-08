2686e55e0d6c561a59c4f65893c6f2e4f1e520f3
Felix <bbaobelief@163.com>
2016-04-08 09:50:03 +0800

    初始化项目

--- charts/urls.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from django.conf.urls import url
-from .views import ChartsView
-
-urlpatterns = [
-    # Charts
-    url(r'^$', ChartsView.as_view(), name='charts_view'),
-]
--- chat/urls.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from django.conf.urls import url
-from .views import ChatView
-
-urlpatterns = [
-    # Chat
-    url(r'^$', ChatView.as_view(), name='chat_view'),
-]
